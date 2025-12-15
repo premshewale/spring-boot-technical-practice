7b6ac5037affdd2249d014b7c1cfc8580025a929
Prem Shewale <premshewale47@gmail.com>
2025-12-15 12:07:59 +0530

    added 2nd part practice

---
+++ Day_2_SpringBoot-1/src/main/java/com/example/demo/Day2SpringBoot1Application.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.example.demo.bean.Student;
+import com.example.demo.service.StudentService;
+
+@SpringBootApplication
+public class Day2SpringBoot1Application {
+
+	public static void main(String[] args) {
+	ApplicationContext ctx=SpringApplication.run(Day2SpringBoot1Application.class, args);
+		
+	StudentService ss =ctx.getBean(StudentService.class);
+	
+//	ss.addStudent(new Student(105,"EEE",90));
+//	ss.addStudent(new Student(106,"FFF",96));
+	ss.addStudent(new Student(110, "ABCD", 99, "Pune"));
+	//ss.displayAll();
+	
+	}
+
+}
